dc70dd3a1527c8b9fe884d94d61f33ebc9a9a952
Ilia Kolev <contact@iliakolev.com>
2014-04-16 01:36:41 +0300

    Revert path
    - fix rbenv path

---
+++ system/path.zsh
@@ -0,0 +1,5 @@
+export PATH="./bin:$HOME/.rbenv/bin:$PATH:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+
+
