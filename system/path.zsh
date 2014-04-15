e1e6faae5b0813e062bfc9aa2bb2a72e03d39748
Ilia Kolev <contact@iliakolev.com>
2014-04-16 01:40:00 +0300

    Remove whitespace

--- system/path.zsh
+++ system/path.zsh
@@ -1,5 +1,2 @@
 export PATH="./bin:$HOME/.rbenv/bin:$PATH:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
-
-
