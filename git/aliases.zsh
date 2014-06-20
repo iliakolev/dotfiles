edf3d6821cf4a2ccf9f49a837880315ecca0c882
Ilia Kolev <contact@iliakolev.com>
2014-06-20 23:49:07 +0300

    Update 'git' aliases.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,12 +7,18 @@ then
 fi
 
 # The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gd='git diff'
+alias g='git'
+alias ga='git add'
+alias gb='git branch'
 alias gc='git commit'
 alias gca='git commit -a'
-alias gco='git checkout'
-alias gb='git branch'
+alias gd='git diff'
+alias gfb='git checkout -b'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gl='git pull --prune'
+alias go='git checkout'
+alias gm='git merge'
+alias gmff='git merge --no-ff'
+alias gp='git push origin HEAD'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gt='git tag'
