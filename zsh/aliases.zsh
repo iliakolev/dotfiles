7f55b6d502299c986cdfc26d046c965d7aea4362
Ilia Kolev <info@iliakolev.com>
2014-09-20 18:31:18 +0300

    Update aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,17 +1,67 @@
-alias r='. ~/.zshrc'
-alias d='cd ~/.dotfiles'
-alias ip='dig +short myip.opendns.com @resolver1.opendns.com'
+# ****************************************************
+#
+#   Aliases
+#
+# ****************************************************
 
-# Vim
-alias v='vim'
+# ----------------------------------------------------
+#   Linux
+# ----------------------------------------------------
 
-# Unix
-alias l='ls'
-alias ll='ls -la'
+## ----- Advanced Packaging Tool ----- ##
+
+alias apti='sudo apt-get install'
+alias aptr='sudo apt-get remove'
+alias apts='sudo apt-cache search'
+alias aptu='sudo apt-get update && \
+            sudo apt-get upgrade'
+
+alias update='aptu'
+
+## ----- Listing ----- ##
+
+# Detect which `ls` flavor is in use
+if ls --color > /dev/null 2>&1; then
+    colorflag='--color'
+else
+    colorflag='-G'
+fi
+
+# List all files colorized in long format
+alias l='ls -1F ${colorflag}'
+
+# List all files colorized in long format, including dot files
+alias la='ls -laF ${colorflag}'
+
+# List only directories
+alias lsd="ls -lF ${colorflag} | grep --color=never '^d'"
+
+## ----- Navigation ----- ##
+
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias .....='cd ../../../..'
 
-# Faster npm for Europeans
-command -v npm > /dev/null && alias npme='npm --registry http://registry.npmjs.eu/'
+## ----- Network ----- ##
+
+alias ip='dig +short myip.opendns.com @resolver1.opendns.com'
+alias localip="ifconfig | \
+               grep 'inet addr' | \
+               grep -v '127.0.0.1' | \
+               cut -d: -f2 | \
+               cut -d' ' -f1"
+
+## ----- Shortcuts ----- ##
+
+alias c='clear'
+alias d='cd ~/.dotfiles'
+alias h='history'
+alias r='. ~/.zshrc'
+alias rm='rm -rf'
+
+# ----------------------------------------------------
+#   Vim
+# ----------------------------------------------------
+
+alias v='vim'
