d047b99e8dd4ece9a03a090c4780700c06c088bf
Ilia Kolev <info@iliakolev.com>
2015-05-23 00:00:22 +0300

    Add "^K" keybind
    Run `^A^K` to delete the current line no matter where I am.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -42,6 +42,7 @@ zle -N newtab
 # handy keybindings
 bindkey '^A' beginning-of-line
 bindkey '^E' end-of-line
+bindkey '^K' kill-line
 bindkey '^R' history-incremental-search-backward
 bindkey '^P' history-search-backward
 bindkey '^Y' accept-and-hold
