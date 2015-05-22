48b625a5442cffe23a76da582c4cab3d1540726a
Ilia Kolev <info@iliakolev.com>
2015-05-22 23:43:47 +0300

    Refactor handy zsh keybindings

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -39,10 +39,11 @@ setopt complete_aliases
 
 zle -N newtab
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
+# handy keybindings
+bindkey '^A' beginning-of-line
+bindkey '^E' end-of-line
+bindkey '^R' history-incremental-search-backward
+bindkey '^P' history-search-backward
+bindkey '^Y' accept-and-hold
+bindkey '^N' insert-last-word
+bindkey -s '^T' '^[Isudo ^[A' # "t" for "toughguy"
