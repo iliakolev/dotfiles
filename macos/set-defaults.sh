d3075cc0adf3612f42e885c619db852edd021c59
Ilia Kolev <iliakolev@users.noreply.github.com>
2018-09-19 23:52:23 +0300

    Display full POSIX path as Finder window title

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -48,6 +48,9 @@ defaults write com.apple.finder ShowStatusBar -bool true
 # Finder: show path bar
 defaults write com.apple.finder ShowPathbar -bool true
 
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
 # Use list view in all Finder windows by default
 # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
