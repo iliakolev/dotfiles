49e87bf1a1d98b2a3771991fb8b91f0526662b00
Ilia Kolev <iliakolev@users.noreply.github.com>
2018-09-19 23:50:06 +0300

    Finder: show path bar

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -45,6 +45,9 @@ defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 # Finder: show status bar
 defaults write com.apple.finder ShowStatusBar -bool true
 
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
 # Use list view in all Finder windows by default
 # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
