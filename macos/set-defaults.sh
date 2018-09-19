ec0ae74d26ed21c7a80837e90702906507e8f71e
Ilia Kolev <iliakolev@users.noreply.github.com>
2018-09-20 00:32:52 +0300

    Remove the animation when hiding/showing the Dock

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -76,3 +76,6 @@ defaults write com.apple.dock autohide -bool true
 
 # Remove the auto-hiding Dock delay
 defaults write com.apple.dock autohide-delay -float 0
+
+# Remove the animation when hiding/showing the Dock
+defaults write com.apple.dock autohide-time-modifier -float 0
